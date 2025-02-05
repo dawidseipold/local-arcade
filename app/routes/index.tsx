f1dd0c71209521bee7cba6c2f946e46b14240d08
dawidseipold <seipold.dawid@gmail.com>
2025-02-05 11:09:58 +0100

    build: init commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,17 @@
+// app/routes/index.tsx
+import { createFileRoute, useRouter } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({
+    component: Home,
+})
+
+function Home() {
+    const router = useRouter()
+    const state = Route.useLoaderData()
+
+    return (
+        <div>
+            Hello World!
+        </div>
+    )
+}
