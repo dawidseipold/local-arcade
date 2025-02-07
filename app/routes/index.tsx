1f110ac3faacaa560b85d7a014ff40ed25ec2de2
dawidseipold <seipold.dawid@gmail.com>
2025-02-07 19:16:20 +0100

    chore: adjust the main page to use new components

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,19 +1,24 @@
 // app/routes/index.tsx
-import { createFileRoute, useRouter } from '@tanstack/react-router'
-import Header from "../components/layout/header/Header.tsx";
+import { createFileRoute, useRouter } from "@tanstack/react-router";
+import Header from "@/components/layout/header/Header";
+import AdBanner from "@/components/layout/ad-banner/AdBanner";
+import { cn } from "@/utils/cn";
 
-export const Route = createFileRoute('/')({
-    component: Home,
-})
+export const Route = createFileRoute("/")({
+  component: Home,
+});
 
 function Home() {
-    const router = useRouter()
-    const state = Route.useLoaderData()
+  const router = useRouter();
+  const state = Route.useLoaderData();
 
-    return (
-        <div className="bg-red-500">
-            <Header />
-            Hello World!
-        </div>
-    )
+  return (
+    <div className={cn("flex flex-col gap-y-8 items-center")}>
+      <Header />
+
+      <div className="flex flex-col gap-y-8 items-center container px-4 md:px-8">
+        <AdBanner />
+      </div>
+    </div>
+  );
 }
